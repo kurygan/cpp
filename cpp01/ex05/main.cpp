41c66ad5f5592aa7deeb0926af923ab48e51ee30
Kurygan <medhy.kettab06@gmail.com>
2025-12-14 00:54:13 +0100

    ex05-cpp01

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkettab <mkettab@student.42mulhouse.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/14 00:36:53 by mkettab           #+#    #+#             */
+/*   Updated: 2025/12/14 00:53:52 by mkettab          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(){
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("CACA");
+	return 0;
+}
