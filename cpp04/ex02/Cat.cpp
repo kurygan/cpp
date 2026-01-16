ac8e49760c3aa8de7cf25781036fe41caf0ddea0
Kurygan <medhy.kettab06@gmail.com>
2026-01-16 23:55:41 +0100

    [FIXED]

--- cpp04/ex02/Cat.cpp
+++ cpp04/ex02/Cat.cpp
@@ -6,7 +6,7 @@ Cat::Cat() {
 	std::cout << "Cat default constructor called" << std::endl;
 }
 Cat::Cat(const Cat &copy) : Animal(copy) {*this = copy; std::cout << "Cat copy constructor called" << std::endl;}
-Cat::~Cat() {std::cout << "Cat destructor called" << std::endl;}
+Cat::~Cat() {delete this->_brain; std::cout << "Cat destructor called" << std::endl;}
 
 Cat		&Cat::operator=(const Cat &copy) {
 	std::cout << "Cat assignment operator called" << std::endl;
--- cpp04/ex02/Dog.cpp
+++ cpp04/ex02/Dog.cpp
@@ -6,7 +6,7 @@ Dog::Dog() {
 	std::cout << "Dog default constructor called" << std::endl;
 }
 Dog::Dog(const Dog &copy) : Animal(copy) {*this = copy; std::cout << "Dog copy constructor called" << std::endl;}
-Dog::~Dog() {std::cout << "Dog destructor called" << std::endl;}
+Dog::~Dog() {delete this->_brain; std::cout << "Dog destructor called" << std::endl;}
 
 Dog		&Dog::operator=(const Dog &copy) {
 	std::cout << "Dog assignment operator called" << std::endl;
