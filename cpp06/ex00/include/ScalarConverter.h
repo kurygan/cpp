804f4d95c08cb3d4e702937400f963691eeb4205
Kurygan <medhy.kettab06@gmail.com>
2026-02-11 22:39:38 +0100

    [TEMP] Start of template conversion

--- cpp06/ex00/include/ScalarConverter.h
+++ cpp06/ex00/include/ScalarConverter.h
@@ -2,8 +2,26 @@
 
 #	include <iostream>
 #	include <string>
-#	include <climits>
+#	include <limits>
 #	include <iomanip>
+
+template <typename T>
+void printConverter(T value){
+	std::cout << "char: ";
+	if (value < std::numeric_limits<char>::min() || value > std::numeric_limits<char>::max())
+		std::cout << "impossible" << std::endl;
+	else if (!std::isprint(static_cast<char>value))
+		std::cout << "non-displayable" << std::endl;
+	else std::cout << "'" << static_cast<char>value << "'" << std::endl;
+
+	std::cout << "int: "
+	if (value < std::numeric_limits<int>::min() || value > std::numeric_limits<int>::max())
+		std::cout << "impossible" << std::endl;
+	else std::cout << static_cast<int>value << std::endl;
+
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>value << "f" << std::endl;
+	std::cout << "double: " << static_cast<double>value << std::endl;
+}
 
 class ScalarConverter {
 	private:
--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -1,56 +1,73 @@
 #include "ScalarConverter.h"
 
-void charConvert(int c) {
-	std::cout << "char: " << char(c) << std::endl;
-	std::cout << "int: " << c << std::endl;
-	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(c) << std::endl;
-	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(c) << "f" << std::endl;
+bool isInt(std::string param){
+	for (int i = 0; i < param.length(); i++)
+		if (!std::isdigit(param[i]))
+			return false;
+	return true;
 }
-//void intConvert(int num) {
-//	std::cout << "char: " << std::endl;
-//	std::cout << "int: " << std::endl;
-//	std::cout << "double: " << std::endl;
-//	std::cout << "float: " << std::endl;
-//}
-//void doubleConvert(double num) {
-//	std::cout << "char: " << std::endl;
-//	std::cout << "int: " << std::endl;
-//	std::cout << "double: " << std::endl;
-//	std::cout << "float: " << std::endl;
-//}
-//void floatConvert(float num) {
-//	std::cout << "char: " << std::endl;
-//	std::cout << "int: " << std::endl;
-//	std::cout << "double: " << std::endl;
-//	std::cout << "float: " << std::endl;
-//}
-void maxConvert() {
-	std::cout << "char: impossible" << std::endl;
-	std::cout << "int: " << __INT_MAX__ << std::endl;
-	std::cout << "double: " << __DBL_MAX__<< std::endl;
-	std::cout << "float: " << __FLT_MAX__ << "f" << std::endl;
+bool isChar(std::string param){
+	if (std::isprint(param[0]) && !std::isdigit(param[0]) && param.length() == 1)
+		return true;
+	return false;
 }
-void minConvert() {
-	std::cout << "char: impossible" << std::endl;
-	std::cout << "int: " << INT_MIN << std::endl;
-	std::cout << "double: " << __DBL_MIN__ << std::endl;
-	std::cout << "float: " << __FLT_MIN__ << "f" << std::endl;
+bool isDouble(std::string param){
+	size_t dotPos = param.find('.');
+	if (dotPos != -1 && param.find('.', dotPos + 1) == -1) {
+		for (size_t i = 0; i < param.length(); i++) {
+			if (!std::isdigit(param[i]) && param[i] != '.')
+				return false;
+		}
+		return true;
+	}
+	return false;
 }
-void nanConvert() {
+bool isFloat(std::string param){
+	size_t dotPos = param.find('.');
+	if (dotPos != -1 && param.find('.', dotPos + 1) == -1) {
+		size_t i = 0;
+		while (i < param.length() - 1) {
+			if (!std::isdigit(param[i]) && param[i] != '.')
+				return false;
+			i++;
+		}
+		if (param[i] == 'f')
+			return true;
+	}
+	return false;
+}
+
+bool minConvert() {
+	std::cout << "char: non-displayable" << std::endl;
+	std::cout << "int: " << std::numeric_limits<int>::min() << std::endl;
+	std::cout << "double: " << std::fixed << std::numeric_limits<double>::min() << std::endl;
+	std::cout << "float: " << std::fixed << std::numeric_limits<float>::min() << "f" << std::endl;
+	return true;
+}
+bool maxConvert() {
+	std::cout << "char: non-displayable" << std::endl;
+	std::cout << "int: " << std::numeric_limits<int>::max() << std::endl;
+	std::cout << "double: " << std::fixed << std::numeric_limits<double>::max() << std::endl;
+	std::cout << "float: " << std::fixed << std::numeric_limits<float>::max() << "f" << std::endl;
+	return true;
+}
+bool nanConvert() {
 	std::cout << "char: non-displayable" << std::endl;
 	std::cout << "int: 0" << std::endl;
 	std::cout << "double: 0.0" << std::endl;
 	std::cout << "float: 0.0f" << std::endl;
+	return true;
 }
 
-void checkChar(std::string param) {
-	if (param.length() == 1) charConvert(param[0]);
-	else if (param == "+inf" || param == "+inff") maxConvert();
-	else if (param == "-inf" || param == "-inff") minConvert();
-	else if (param == "nan" || param == "nanf") nanConvert();
+bool isSpecial(std::string param){
+	if (param == "-inf" || param == "-inff") return minConvert();
+	else if (param == "+inf" || param == "+inff") return maxConvert();
+	else if (param == "nan" || param == "nanf") return nanConvert();
+	return false;
 }
 
 void ScalarConverter::convert(std::string param) {
-	if (!std::isprint(param[0])) {std::cerr << "Error, Use: ./converter [num or char]"; return;}
-	if (std::isprint(param[0]) && !std::isdigit(param[0])) checkChar(param);
+	if (isChar(param)) printConverter(static_cast<char>(param[0]));
+	else if (isInt(param)) printConverter(static_cast<int>(atoi(param.c_str()));
+	else if ()
 }
