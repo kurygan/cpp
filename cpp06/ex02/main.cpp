5bfbd7bf6b86ec0b70576edcab341525244d4c2a
Kurygan <medhy.kettab06@gmail.com>
2026-03-03 23:43:51 +0100

    [FIX] ex02-cpp06

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,19 @@
+#include "Base.h"
+
+int main() {
+	std::srand(static_cast<unsigned int>(std::time(0)));
+
+	for (int i = 0; i < 5; i++) {
+		std::cout << "\nTest " << i + 1 << ":" << std::endl;
+
+		Base *ptr = generate();
+		std::cout << "Pointer Identification: "; identify(ptr);
+		std::cout << "Reference Identification: "; identify(*ptr);
+		delete ptr;
+	}
+
+	std::cout << "NULL Pointer Test:" << std::endl;
+	identify(static_cast<Base*>(NULL));
+
+	return 0;
+}
